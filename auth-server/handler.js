1bce98ba852e2725f41dcb14c34bcafebb216df8
unknown <reman1999@outlook.com>
2023-03-15 17:31:57 +0100

    Revert URI link change

--- auth-server/handler.js
+++ auth-server/handler.js
@@ -12,7 +12,7 @@ const credentials = {
   auth_uri: 'https://accounts.google.com/o/oauth2/auth',
   token_uri: 'https://oauth2.googleapis.com/token',
   auth_provider_x509_cert_url: 'https://www.googleapis.com/oauth2/v1/certs',
-  redirect_uris: ['https://remanbalak.github.io/Meet-App'],
+  redirect_uris: ['https://remanbalak.github.io/Meet-App/'],
   javascript_origins: ['https://remanbalak.github.io', 'http://localhost:3000'],
 };
 const { client_secret, client_id, redirect_uris, calendar_id } = credentials;
