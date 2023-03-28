68aeeb545266023dc370d503827eeea7186eacd1
unknown <reman1999@outlook.com>
2023-03-28 23:55:24 +0200

    Updated CSS for Welcomescreen

--- src/NumberOfEvents.js
+++ src/NumberOfEvents.js
@@ -1,18 +1,18 @@
-import React, { Component } from "react";
-import { ErrorAlert } from "./Alert";
+import React, { Component } from 'react';
+import { ErrorAlert } from './Alert';
 
 class NumberOfEvents extends Component {
   state = {
     num: 32,
-    errorText: "",
+    errorText: '',
   };
 
   changeNum = (value) => {
     this.setState({ num: value });
     this.props.updateNumberOfEvents(value);
     if (value < 1 || value > 32) {
-      this.setState({ errorText: "Select number from 1 to 32" });
-    } else this.setState({ errorText: "" });
+      this.setState({ errorText: 'Select number from 1 to 32' });
+    } else this.setState({ errorText: '' });
   };
 
   componentDidMount() {
@@ -25,7 +25,7 @@ class NumberOfEvents extends Component {
     return (
       <div>
         <ErrorAlert text={errorText} />
-        <label>
+        <label className="Eventslabel">
           Number of events
           <input
             className="num"
